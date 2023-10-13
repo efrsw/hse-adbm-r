769645d8f5684401616934e22afc001cfcbb860a
Egor Fursov <fursov.egor@gmail.com>
2023-10-13 13:00:00 +0300

    Made function

--- seminar_5/hw5.R
+++ seminar_5/hw5.R
@@ -4,22 +4,26 @@ n_max = function(vec, n) {
 
 ####
 
-x0 = matrix(seq(-1, 2, length.out=1000), ncol = 1000, nrow = 1000, byrow = TRUE)
-y0 = matrix(seq(-1, 1, length.out=1000), ncol = 1000, nrow = 1000, byrow = FALSE)
-
-x = x0
-y = y0
-
-for(i in 1:20) {
-  xt = x
-  yt = y
+mb = function(X, Y, dimX, dimY, n) {
   
-  x=xt^2-yt^2+x0
-  y=2*xt*yt+y0
+  x0 = matrix(seq(X[1], X[2], length.out=dimX), ncol = dimX, nrow = dimX, byrow = TRUE)
+  y0 = matrix(seq(Y[1], Y[2], length.out=dimY), ncol = dimY, nrow = dimY, byrow = FALSE)
+  
+  x = x0
+  y = y0
+  
+  for(i in 1:n) {
+    xt = x
+    yt = y
+    
+    x=xt^2-yt^2+x0
+    y=2*xt*yt+y0
+  }
+  
+  z = t(abs(x^2 + y^2))
+  z[!is.na(z)] = rank(z[!is.na(z)])
+  image(z^3, col=rev(terrain.colors(dimX)))
 }
 
-z = t(abs(x^2 + y^2))
-z[!is.na(z)] = rank(z[!is.na(z)])
-image(z^3, col=rev(terrain.colors(1000)))
-
+mb(c(-2, 2), c(-3, 3), 1500, 1500, 15)
 ####
